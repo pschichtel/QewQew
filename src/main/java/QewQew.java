fcab35bc5d8d9a55ac499237ea1dc8b235200a5c
Phillip Schichtel <phillip@schich.tel>
2018-11-02 03:10:15 +0100

    stuff and things

--- src/main/java/QewQew.java
+++ src/main/java/QewQew.java
@@ -26,11 +26,12 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.DSYNC;
 import static java.nio.file.StandardOpenOption.READ;
@@ -53,205 +54,324 @@ public class QewQew implements Closeable {
     private static final int ENTRY_HEADER_SIZE = Short.BYTES;
     private static final int NULL_REF = 0;
     private static final String SUFFIX = ".qew";
+    private static final int MAX_ID = ((short)-1) & 0xFFFF;
 
     private final Path prefix;
     private Path headPath;
-    private Path tailPath;
 
     private FileChannel headFile;
+    private final Lock headLock;
     private FileChannel tailFile;
+    private final Lock tailLock;
 
     private final long chunkSize;
     private final long blockSize;
     private final ByteBuffer headBuffer;
     private final ByteBuffer tailBuffer;
 
-    public QewQew(Path prefix, int blockSize, long chunkSize) throws IOException {
-        this.prefix = prefix;
-        this.blockSize = blockSize;
-        this.chunkSize = chunkSize;
-        this.headPath = findHead(prefix);
-        this.tailPath = findTail(prefix);
+    public QewQew(Path headPath, int blockSize, long chunkSize, boolean fair) throws IOException {
+
+        this.headLock = new ReentrantLock(fair);
+        this.tailLock = new ReentrantLock(fair);
 
         this.headBuffer = ByteBuffer.allocateDirect(blockSize);
         this.tailBuffer = ByteBuffer.allocateDirect(blockSize);
+
+        this.prefix = headPath.toAbsolutePath().getParent();
+        this.blockSize = blockSize;
+        this.chunkSize = chunkSize;
+        this.headPath = headPath;
         this.headFile = openChunk(this.headPath);
-        if (this.headPath.equals(this.tailPath)) {
-            this.tailFile = this.headFile;
-        } else {
-            this.tailFile = openChunk(this.tailPath);
+
+        this.tailFile = findTail(prefix, this.headFile, this.headBuffer);
+    }
+
+    private static int readNextRef(FileChannel file, ByteBuffer buf) throws IOException {
+        if (file.size() == 0) {
+            return NULL_REF;
+        }
+        buf.clear().limit(CHUNK_HEADER_SIZE);
+        file.position(0);
+        file.read(buf);
+        buf.flip();
+        buf.getLong();
+        buf.getLong();
+        return buf.getShort();
+    }
+
+    private static FileChannel findTail(Path prefix, FileChannel file, ByteBuffer buf) throws IOException {
+        try (FileLock ignored = file.lock()) {
+            int nextRef = readNextRef(file, buf);
+            Path next = resolveNextRef(prefix, nextRef);
+            FileChannel nextFile = tryOpenChunk(next);
+            if (nextFile == null) {
+                return file;
+            } else {
+                // it is important here to continue the search file the current file is still locked to prevent
+                // queue changes while search.
+                return findTail(prefix, nextFile, buf);
+            }
         }
     }
 
-    private static Path findHead(Path prefix) throws IOException {
-        Optional<Path> first = findFiles(prefix).min(Path::compareTo);
-        return first.orElseGet(() -> initial(prefix));
-    }
-
-    private static Path findTail(Path prefix) throws IOException {
-        Optional<Path> first = findFiles(prefix).min((a, b) -> -a.compareTo(b));
-        return first.orElseGet(() -> initial(prefix));
-    }
-
-    private static Path initial(Path prefix) {
-        return prefix.resolve("1" + SUFFIX);
-    }
-
-    private static Stream<Path> findFiles(Path prefix) throws IOException {
-        return Files.list(prefix).filter(QewQew::isQueueFile);
-    }
-
-    private static boolean isQueueFile(Path p) {
-        if (!Files.isRegularFile(p, LinkOption.NOFOLLOW_LINKS)) {
-            return false;
-        }
-
-        return p.endsWith(SUFFIX);
+    private static Path resolveNextRef(Path prefix, int id) {
+        return prefix.resolve((id % MAX_ID) + SUFFIX);
     }
 
     private static FileChannel openChunk(Path path) throws IOException {
         return FileChannel.open(path, CREATE, WRITE, READ, SYNC, DSYNC);
     }
 
+    private static FileChannel tryOpenChunk(Path path) throws IOException {
+        try {
+            return FileChannel.open(path, WRITE, READ, SYNC, DSYNC);
+        } catch (NoSuchFileException e) {
+            return null;
+        }
+    }
+
     public boolean isEmpty() throws IOException {
-        try (FileLock ignored = headFile.lock()) {
-            if (headFile.size() == 0) {
-                return true;
+        headLock.lock();
+        try {
+            try (FileLock ignored = headFile.lock()) {
+                return findHeadPtr() == -1;
             }
-            headFile.position(0);
-            headBuffer.clear().limit(CHUNK_HEADER_SIZE);
-            headFile.read(headBuffer);
-            headBuffer.flip();
-            long headPtr = headBuffer.getLong();
-            long tailPtr = headBuffer.getLong();
-            return headPtr >= tailPtr;
+        } finally {
+            headLock.unlock();
         }
     }
 
     public void clear() throws IOException {
-        try (FileLock ignored = headFile.lock()) {
-            headFile.truncate(0);
+        headLock.lock();
+        try {
+            try (FileLock ignored = headFile.lock()) {
+                headFile.truncate(0);
+            }
+        } finally {
+            headLock.unlock();
         }
+    }
+
+    @ExpectsValidLock
+    private long findHeadPtr() throws IOException {
+        if (headFile.size() == 0) {
+            return -1;
+        }
+
+        headFile.position(0);
+        headBuffer.clear().limit(CHUNK_HEADER_SIZE);
+        headFile.read(headBuffer);
+        headBuffer.flip();
+
+        long headPtr = headBuffer.getLong();
+        long tailPtr = headBuffer.getLong();
+        return headPtr < tailPtr ? headPtr : -1;
+    }
+
+    private FileLock lockNonEmptyHead() throws IOException {
+        long headPtr;
+        long tailPtr;
+        int nextRef;
+        FileLock lock = headFile.lock();
+        while (true) {
+            if (headFile.size() == 0) {
+                break;
+            }
+            headFile.position(0);
+            headBuffer.clear().limit(CHUNK_HEADER_SIZE);
+            headFile.read(headBuffer);
+            headBuffer.flip();
+            headPtr = headBuffer.getLong();
+            tailPtr = headBuffer.getLong();
+            if (headPtr < tailPtr) {
+                break;
+            }
+
+            nextRef = headBuffer.getShort() & 0xFFFF;
+            Path next = resolveNextRef(prefix, nextRef);
+            try {
+                Files.move(next, headPath, ATOMIC_MOVE);
+                this.headFile = openChunk(headPath);
+                lock = this.headFile.lock();
+            } catch (NoSuchFileException ignored) {
+                // next does not exist,
+                break;
+            }
+        }
+        return lock;
     }
 
     public byte[] peek() throws IOException {
-        try (FileLock ignored = headFile.lock()) {
-            if (headFile.size() == 0) {
+        final FileLock currentLock = lockNonEmptyHead();
+        try {
+            long headPtr = findHeadPtr();
+            if (headPtr == -1) {
                 return null;
             }
 
-            headFile.position(0);
-            headBuffer.clear().limit(CHUNK_HEADER_SIZE);
-            headFile.read(headBuffer);
-            headBuffer.flip();
-            long headPtr = headBuffer.getLong();
-            long tailPtr = headBuffer.getLong();
-            if (headPtr >= tailPtr) {
-                // TODO delete empty head
-                return null;
+            return readEntry(headFile, headBuffer, headPtr);
+        } finally {
+            if (currentLock.isValid()) {
+                currentLock.release();
             }
-            headFile.position(headPtr);
-
-            headBuffer.clear().limit(ENTRY_HEADER_SIZE);
-            headFile.read(headBuffer);
-            headBuffer.flip();
-            final int length = headBuffer.getShort() & 0xFFFF;
-
-            int offset = 0;
-            byte[] output = new byte[length];
-            while (offset < length) {
-                headBuffer.clear();
-                headBuffer.limit(Math.min(length - offset, headBuffer.capacity()));
-                headFile.read(headBuffer);
-                headBuffer.flip();
-                headBuffer.get(output, offset, headBuffer.limit());
-                offset += headBuffer.limit();
-            }
-
-            return output;
         }
     }
 
-    public boolean deqew() throws IOException {
-        try (FileLock ignored = headFile.lock()) {
-            headFile.position(0);
-            headBuffer.clear().limit(CHUNK_HEADER_SIZE);
-            headFile.read(headBuffer);
-            headBuffer.flip();
-            long headPtr = headBuffer.getLong();
-            long tailPtr = headBuffer.getLong();
-            int nextRef = headBuffer.getShort() & 0xFFFF;
-            if (headPtr >= tailPtr) {
-                // TODO delete empty head
-                return false;
-            }
-            headFile.position(headPtr);
+    @ExpectsValidLock
+    private static byte[] readEntry(FileChannel file, ByteBuffer buf, long ptr) throws IOException {
+        file.position(ptr);
 
-            headBuffer.clear().limit(ENTRY_HEADER_SIZE);
-            headFile.read(headBuffer);
-            headBuffer.flip();
-            final int length = headBuffer.getShort() & 0xFFFF;
-            updateHeader(headFile, headBuffer, headFile.position() + length, tailPtr, nextRef);
-            return true;
+        buf.clear().limit(ENTRY_HEADER_SIZE);
+        file.read(buf);
+        buf.flip();
+        final int length = buf.getShort() & 0xFFFF;
+
+        int offset = 0;
+        byte[] output = new byte[length];
+        while (offset < length) {
+            buf.clear();
+            buf.limit(Math.min(length - offset, buf.capacity()));
+            file.read(buf);
+            buf.flip();
+            buf.get(output, offset, buf.limit());
+            offset += buf.limit();
+        }
+
+        return output;
+    }
+
+    public boolean deqew() throws IOException {
+        headLock.lock();
+        try {
+            try (FileLock ignored = headFile.lock()) {
+                headFile.position(0);
+                headBuffer.clear().limit(CHUNK_HEADER_SIZE);
+                headFile.read(headBuffer);
+                headBuffer.flip();
+                long headPtr = headBuffer.getLong();
+                long tailPtr = headBuffer.getLong();
+                int nextRef = headBuffer.getShort() & 0xFFFF;
+                if (headPtr >= tailPtr) {
+                    // TODO delete empty head
+                    return false;
+                }
+                headFile.position(headPtr);
+
+                headBuffer.clear().limit(ENTRY_HEADER_SIZE);
+                headFile.read(headBuffer);
+                headBuffer.flip();
+                final int length = headBuffer.getShort() & 0xFFFF;
+                updateHeader(headFile, headBuffer, headFile.position() + length, tailPtr, nextRef);
+                return true;
+            }
+        } finally {
+            headLock.unlock();
         }
     }
 
     public void enqew(byte[] input) throws IOException {
-        try (FileLock ignored = tailFile.lock()) {
-            final long headPtr;
-            final long tailPtr;
-            final int nextRef;
-            if (tailFile.size() == 0) {
-                headPtr = CHUNK_HEADER_SIZE;
-                tailPtr = CHUNK_HEADER_SIZE;
-                nextRef = NULL_REF;
-                tailFile.truncate(chunkSize);
-            } else {
-                tailFile.position(0);
-                tailBuffer.clear().limit(CHUNK_HEADER_SIZE);
-                tailFile.read(tailBuffer);
-                tailBuffer.flip();
-                headPtr = tailBuffer.getLong();
-                tailPtr = tailBuffer.getLong();
-                nextRef = tailBuffer.getShort() & 0xFFFF;
-            }
-            tailFile.position(tailPtr);
+        tailLock.lock();
+        try {
+            try (FileLock ignored = tailFile.lock()) {
+                final long headPtr;
+                final long tailPtr;
+                final int nextRef;
+                if (tailFile.size() == 0) {
+                    headPtr = CHUNK_HEADER_SIZE;
+                    tailPtr = CHUNK_HEADER_SIZE;
+                    nextRef = NULL_REF;
+                    tailFile.truncate(chunkSize * blockSize);
+                } else {
+                    tailFile.position(0);
+                    tailBuffer.clear().limit(CHUNK_HEADER_SIZE);
+                    tailFile.read(tailBuffer);
+                    tailBuffer.flip();
+                    headPtr = tailBuffer.getLong();
+                    tailPtr = tailBuffer.getLong();
+                    nextRef = tailBuffer.getShort() & 0xFFFF;
+                }
+                tailFile.position(tailPtr);
 
-            tailBuffer.clear();
-            tailBuffer.putShort((short)(input.length & 0xFFFF));
-            tailBuffer.flip();
-            tailFile.write(tailBuffer);
-
-            int offset = 0;
-            while (offset < input.length) {
                 tailBuffer.clear();
-                tailBuffer.put(input, offset, Math.min(tailBuffer.capacity(), input.length - offset));
+                tailBuffer.putShort((short) (input.length & 0xFFFF));
                 tailBuffer.flip();
                 tailFile.write(tailBuffer);
-                offset += tailBuffer.limit();
-            }
 
-            updateHeader(tailFile, tailBuffer, headPtr, tailFile.position(), nextRef);
+                int offset = 0;
+                while (offset < input.length) {
+                    tailBuffer.clear();
+                    tailBuffer.put(input, offset, Math.min(tailBuffer.capacity(), input.length - offset));
+                    tailBuffer.flip();
+                    tailFile.write(tailBuffer);
+                    offset += tailBuffer.limit();
+                }
+
+                updateHeader(tailFile, tailBuffer, headPtr, tailFile.position(), nextRef);
+            }
+        } finally {
+            tailLock.unlock();
         }
     }
 
+    private FileLock lockTail(int forBytes) throws IOException {
+        long headPtr;
+        long tailPtr;
+        int nextRef;
+        FileLock lock = tailFile.lock();
+        while (true) {
+            if (headFile.size() == 0) {
+                break;
+            }
+            headFile.position(0);
+            headBuffer.clear().limit(CHUNK_HEADER_SIZE);
+            headFile.read(headBuffer);
+            headBuffer.flip();
+            headPtr = headBuffer.getLong();
+            tailPtr = headBuffer.getLong();
+            nextRef = headBuffer.getShort() & 0xFFFF;
+            if (nextRef != NULL_REF && tailPtr + forBytes < tailFile.size()) {
+                break;
+            }
+            Path next = resolveNextRef(prefix, nextRef);
+            try {
+                Files.move(next, headPath, ATOMIC_MOVE);
+                this.headFile = openChunk(headPath);
+                lock = this.headFile.lock();
+            } catch (NoSuchFileException ignored) {
+                // next does not exist,
+                break;
+            }
+        }
+        return lock;
+    }
+
     private static void updateHeader(FileChannel ch, ByteBuffer buf, long headPtr, long tailPtr, int next) throws IOException {
+        short wrappedNext = (short) (next & 0xFFFF);
+        if (wrappedNext == NULL_REF) {
+            wrappedNext++;
+        }
         ch.position(0);
         buf.clear();
         buf.putLong(headPtr);
         buf.putLong(tailPtr);
-        buf.putShort((short)(next & 0xFFFF));
+        buf.putShort(wrappedNext);
         buf.flip();
         ch.write(buf);
     }
 
     @Override
     public void close() throws IOException {
-        FileLock headLock = headFile.lock();
-        if (!headLock.channel().equals(tailFile)) {
-            tailFile.lock();
-            tailFile.close();
+        headLock.lock();
+        tailLock.lock();
+        try {
+            try {
+                this.headFile.close();
+            } finally {
+                this.tailFile.close();
+            }
+        } finally {
+            headLock.unlock();
+            tailLock.unlock();
         }
-        headFile.close();
     }
 }
--- src/test/java/QewQewTest.java
+++ src/test/java/QewQewTest.java
@@ -23,20 +23,21 @@
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QewQewTest {
 
-    static final Path HERE = Paths.get(".");
+    static final Path HEAD_PATH = Paths.get("qew-head");
 
     public static final int BLOCK_SIZE = 4096;
     public static final int CHUNK_SIZE = 1024 * 1024;
 
     @Test
     void testQueue() throws IOException {
-        try (QewQew q = new QewQew(HERE, BLOCK_SIZE, CHUNK_SIZE)) {
+        try (QewQew q = new QewQew(HEAD_PATH, BLOCK_SIZE, CHUNK_SIZE, true)) {
             q.clear();
             assertTrue(q.isEmpty());
 
@@ -48,6 +49,17 @@ class QewQewTest {
 
             assertTrue(q.deqew());
             assertTrue(q.isEmpty());
+        }
+    }
+
+    @Test
+    void testManyWrites() throws IOException {
+        Random r = new Random(1);
+        try (QewQew q = new QewQew(HEAD_PATH, BLOCK_SIZE, CHUNK_SIZE, true)) {
+            q.clear();
+            for (int i = 0; i < 1000; ++i) {
+                q.enqew(buf(r.nextInt(((short) -1) & 0xFFFF)));
+            }
         }
     }
 
